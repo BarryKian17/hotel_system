e6c58bb55d119d092e40f67867be51d8e436b38d
kaung set linn <kaungsetisking@gmail.com>
2023-09-22 08:48:17 +0630

    admin and user auth

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            // Admin
+            [
+                'name'=>'admin',
+                'email'=>'admin@gmail.com',
+                'password'=>Hash::make('barry1710'),
+                'role'=>'admin',
+                'status'=>'active',
+            ],
+            // User
+            [
+                'name'=>'user',
+                'email'=>'user@gmail.com',
+                'password'=>Hash::make('barry1710'),
+                'role'=>'user',
+                'status'=>'active',
+            ],
+        ]);
+    }
+}
